34a1a833949ddef1ba3d2831406809959f8da5a6
Pascal Laenen <pascal@Pascals-MacBook-Pro.local>
2016-03-15 13:56:52 +0000

    added tests

---
+++ index.js
@@ -0,0 +1,73 @@
+var AwsHelper = require('aws-lambda-helper');
+var tableName = "numo-tagable";
+var _ = require('lodash');
+
+var internal = {};
+
+exports.handler = function (event , context){
+    AwsHelper.init(context);
+
+    //Check if an ID is provided
+    if (!event._id) {return context.fail(new Error('no _id provided'))};
+
+    internal.processEvent(event, function(err,data){
+        if (err) return context.fail(err); // an error occurred
+        return context.succeed(data); // successful response
+    });
+}
+
+internal.processEvent = function(event, cb){
+    var doc = internal.initDoc(event);
+
+    var tags = getTags(doc.tags)
+    //Insert document in DB
+    AwsHelper.DynamoDB.putItem({
+            TableName: tableName,
+            Item: {
+                _id: {S: doc._id.split('').reverse().join('')}, //reverse the id, better for distribution over the dynamodb cluster
+                displayName: {
+                    S: doc.displayName
+                },
+                activeTags: {
+                    SS: tags.active
+                },
+                disabledTags: {
+                    SS: tags.disabled
+                },
+                doc: {
+                    S: JSON.stringify(doc)
+                }
+            }
+        }, 
+        function(err, data){
+            return cb(err, data);
+        }
+    );    
+
+    function getTags(tags){
+        var result = {
+            active: [],
+            disabled: []
+        };
+        tags.forEach(function (item){
+            if (item.active){
+                result.active.push(item.tagId)
+            }
+            else {
+                result.disabled.push(item.tagId)
+            }
+        });
+        return result;
+    }
+}
+
+internal.initDoc = function (event){
+    return  {
+        _id: event._id,
+        displayName: event.displayName || event._id,
+        tags: event.tags || [],
+        content: event.content || []
+    };
+};
+
+exports._internal = internal; 
---
+++ test/eventFixtures.js
@@ -0,0 +1,30 @@
+exports.getEvent = function(){
+  return {
+    _id: "foo-id",
+    displayName: "foo-display-name",
+    tags: [    
+      {
+        tagId: "geography:geonames.123",
+        tagType: "geography",
+        source: "tag:source.user.12234",
+        active: true
+      },
+      {
+        tagId: "geography:geonames.125",
+        tagType: "geography",
+        source: "tag:source.user.12235",
+        active: false
+      }
+    ],
+    content: [
+      {
+        key: "label:en",
+        values: ["Hotel ABC"]
+      },
+      {
+        key: "search:en",
+        values: ["Hotel ABC"]
+      }
+    ] 
+  };
+};
