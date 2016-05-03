80ac0256c9ddccc3dc2732c3f915112638b0fc6f
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-03 12:02:14 +0100

    remove config.json as no longer required

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cloudsearch: {
-    ci: 'https://doc-taggable-ci-dm62zw5dvzelh2nbvr3ao276zy.eu-west-1.cloudsearch.amazonaws.com'
-  },
-  lambda: 'lambda-taggable-inheritance-indexer-v1'
-};
