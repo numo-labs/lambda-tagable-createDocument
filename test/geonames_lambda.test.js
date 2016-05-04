2e4f63c6728e8cedede25bb5d57248fa959b8d3b
nelsonic <contact.nelsonic+github@gmail.com>
2016-05-04 18:49:57 +0100

    [WiP] geonames lambda is timing out... it was working 5 mins ago...

---
+++ test/geonames_lambda.test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var geo = require('../lib/geonames_lambda');
+var test_hotel_tag = require('./fixtures/test_hotel_tag');
+
+describe('invoke the Geonames Lambda (tag-e-geo)', function () {
+  it('invokes the lambda with sample test_hotel_tag', function (done) {
+    geo(test_hotel_tag, function (err, data) {
+      if (err) console.log(err);
+      var res = JSON.parse(data.Payload);
+      // console.log(err, data.Payload);
+      assert(res[0].displayName === 'Earth');
+      done();
+    });
+  });
+
+  it('invokes the lambda invalid tag (no location.lat)', function (done) {
+    var bad_tag = {_id: 'bad'};
+    geo(bad_tag, function (err, data) {
+      if (err) console.log(err);
+      console.log(err, data);
+      done();
+    });
+  });
+});
--- test/handler.test.js
+++ test/handler.test.js
@@ -1,20 +1,49 @@
 var assert = require('assert');
 var handler = require('../lib/handler.js');
-var mock_event = require('./fixtures/mock_event.json');
+var mock_event = require('./fixtures/test_hotel_tag.json');
 
-describe('Handler functions', function () {
+describe('lib/handler.js', function () {
   it('initTagDoc: should create a doc object from the event', function (done) {
-    var doc = handler.initTagDoc(mock_event);
-    assert.deepEqual(doc, mock_event);
-    done();
+    handler.initTagDoc(mock_event, function (err, doc) {
+      if (err) console.log(err);
+      assert.deepEqual(doc, mock_event);
+      done();
+    });
   });
+
   it('initTagDoc: should use the id as the displayName, tags and metadata defaulted to []', function (done) {
     var event = { _id: '12345' };
-    var doc = handler.initTagDoc(event);
-    assert.deepEqual(Object.keys(doc), ['_id', 'location', 'displayName', 'tags', 'metadata', 'content', 'markets']);
-    assert.deepEqual(doc.tags, []);
-    assert.deepEqual(doc.metadata, []);
-    assert.equal(doc.displayName, event._id);
-    done();
+    handler.initTagDoc(event, function (err, doc) {
+      if (err) console.log(err);
+      assert.deepEqual(Object.keys(doc), ['_id', 'location', 'displayName', 'tags', 'metadata', 'content', 'markets']);
+      assert.deepEqual(doc.tags, []);
+      assert.deepEqual(doc.metadata, []);
+      assert.equal(doc.displayName, event._id);
+      done();
+    });
   });
+
+  it('initTagDoc: should lookup the Geonames Tag if not set', function (done) {
+    var event = { _id: 'test:hotel:mhid.12345', location: { lat: 52.9875, lon: 1.8865 } };
+    handler.initTagDoc(event, function (err, doc) {
+      if (err) console.log(err);
+      console.log(doc);
+      assert.deepEqual(Object.keys(doc), ['_id', 'location', 'displayName', 'tags', 'metadata', 'content', 'markets']);
+      assert.equal(doc.tags.length, 1);
+      assert.equal(doc.tags[0].node, 'geo:geonames.2634643');
+      assert.equal(doc.displayName, event._id);
+      done();
+    });
+  });
+  // it.only('initTagDoc: should lookup the Geonames Tag if not set', function (done) {
+  //   var event = require('./fixtures/test_hotel_with_non_geo_tags.json');
+  //   handler.initTagDoc(event, function (err, doc) {
+  //     if (err) console.log(err);
+  //     console.log(doc);
+  //     assert.deepEqual(Object.keys(doc), ['_id', 'location', 'displayName', 'tags', 'metadata', 'content', 'markets']);
+  //     assert.equal(doc.tags.length, 1);
+  //     assert.equal(doc.tags[1].node, 'geo:geonames.2634643');
+  //     done();
+  //   });
+  // });
 });
