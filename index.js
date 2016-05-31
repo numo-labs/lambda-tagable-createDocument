136530b7458e4cb24f8ef5a7457d72921184c782
hveldstra <h@veldstra.org>
2016-05-31 13:52:39 +0100

    Use callback rather than context.fail/succeed

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ require('env2')('.env'); // Load Environment Variables - github.com/numo-labs/aw
 var AwsHelper = require('aws-lambda-helper');
 var handler = require('./lib/handler.js');
 
-exports.handler = function (event, context) {
+exports.handler = function (event, context, callback) {
   if (!event._id) { // Check if an tag id is provided
-    return context.fail(new Error('no _id provided'));
+    return callback(new Error('no _id provided'));
   }
 
   AwsHelper.init(context, event);
 
   handler.initTagDoc(event, function (err, newTagDoc) {
     if (err) {
-      return context.fail(err);
+      return callback(err);
     }
 
     handler.s3Create(newTagDoc, function (err, data) {
       if (err) {
-        return context.fail(err);
+        return callback(err);
       }
 
-      AwsHelper.log.info({ tag: {eventId: event._id, location: data.Location} }, 'Tag saved to s3');
-      return context.succeed(newTagDoc);
+      AwsHelper.log.info({
+        tag: {
+          eventId: event._id,
+          location: data.Location
+        }
+      }, 'Tag saved to s3');
+      return callback(null, newTagDoc);
     });
   });
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,5 @@
 var assert = require('assert');
 var index = require('../index');
-var utils = require('aws-lambda-test-utils');
-var mockContextCreator = utils.mockContextCreator;
 var testHotelTag = require('./fixtures/test_hotel_tag.json');
 var ctxOpts = {
   invokedFunctionArn: 'arn:aws:lambda:eu-west-1:123456789:function:LambdaTest:ci'
@@ -11,37 +9,39 @@ var handler = require('../lib/handler');
 
 describe('Index handler tests', function () {
   it('Context.fail: called when no id is provided in the event', function (done) {
-    function test (err) {
+    function callback (err, result) {
       assert.equal(err.message, 'no _id provided');
       done();
     }
-    var context = mockContextCreator({}, test);
-    index.handler({}, context);
+    var context = {};
+    index.handler({}, context, callback);
   });
 
   it('Context.succeed: called with the newTagDoc', function (done) {
     var context = {
-      succeed: function (result) { // here's your test:
-        assert.deepEqual(result, testHotelTag);
-        done();
-      },
       invokedFunctionArn: ctxOpts.invokedFunctionArn
     };
-    index.handler(testHotelTag, context);
+    function callback (err, result) {
+      assert(!err);
+      assert.deepEqual(result, testHotelTag);
+      done();
+    }
+    index.handler(testHotelTag, context, callback);
   });
 
   it('Lambda should fail if a tag doc fails to be created', function (done) {
     var mock = sinon.mock(handler);
     mock.expects('initTagDoc').once().callsArgWith(1, new Error('fake initTagDoc error'));
     var context = {
-      invokedFunctionArn: ctxOpts.invokedFunctionArn,
-      fail: function (err) {
-        assert(err);
-        mock.verify();
-        return done();
-      }
+      invokedFunctionArn: ctxOpts.invokedFunctionArn
     };
-    index.handler(testHotelTag, context);
+    function callback (err, result) {
+      assert(err, 'should have an error');
+      console.log(result);
+      mock.verify();
+      return done();
+    }
+    index.handler(testHotelTag, context, callback);
   });
 
   it('Lambda should fail if uploading to S3 fails', function (done) {
@@ -49,13 +49,15 @@ describe('Index handler tests', function () {
     mock.expects('s3Create').once().callsArgWith(1, new Error('fake s3Create error'));
 
     var context = {
-      invokedFunctionArn: ctxOpts.invokedFunctionArn,
-      fail: function (err) {
-        assert(err);
-        mock.verify();
-        return done();
-      }
+      invokedFunctionArn: ctxOpts.invokedFunctionArn
     };
-    index.handler(testHotelTag, context);
+
+    function callback (err, result) {
+      assert(err, 'should have an error');
+      mock.verify();
+      return done();
+    }
+
+    index.handler(testHotelTag, context, callback);
   });
 });
